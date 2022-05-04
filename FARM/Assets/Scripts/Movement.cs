833ed89fcc2d858ff6bdc90d99648e6af0697e84
Prako06 <anhkhoiphamtran21@gmail.com>
2022-05-04 22:09:52 +0700

    Prako

--- FARM/Assets/Scripts/Movement.cs
+++ FARM/Assets/Scripts/Movement.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +13,26 @@ public class Movement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, vertical);
+        AnimateMovement(direction);
 
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if(animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("IsMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("IsMoving", false);
+            }
+        }
     }
 }
